cc6d0d71c62f086e18a94df9611785c1ba2b3546
asher-an <asher.an@kakaocorp.com>
2026-02-04 13:25:29 +0900

    질문타입 enum 추가

---
+++ src/main/java/com/example/entity/QuestionType.java
@@ -0,0 +1,19 @@
+package com.example.entity;
+
+/**
+ * 시험 문제 유형
+ */
+public enum QuestionType {
+    OBJECTIVE("객관식"),
+    SUBJECTIVE("주관식");
+
+    private final String displayName;
+
+    QuestionType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
