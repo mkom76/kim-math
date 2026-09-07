2d7db5da6a77e571da920ef09ac77860cb392202
asher-an <asher.an@kakaocorp.com>
2026-09-08 01:00:25 +0900

    test: verify enrollment during consent registration

--- src/test/java/com/example/config/DataInitializerTest.java
+++ src/test/java/com/example/config/DataInitializerTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.tuple;
 
 @SpringBootTest
-@ActiveProfiles({"test", "local"})
+@ActiveProfiles({"local", "test"})
 @Transactional
 class DataInitializerTest {
 
